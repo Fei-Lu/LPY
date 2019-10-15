c987ff1a3b893cda2d9912f346ba3811fe04fdad
Fei Lu <dr.lufei@gmail.com>
2019-10-15 22:02:06 +0800

    hello world

---
+++ src/HelloWorld.py
@@ -0,0 +1,37 @@
+print("Hello, world!")
+
+# annotation1
+
+'''
+annotation2
+'''
+
+"""
+annotation3
+"""
+
+if True:
+    print("yes")
+else:
+    print("no") #indent
+print("ok")
+
+word = 'string'
+sentence = 'this is a sentence'
+paragraph = '''this
+            is
+            a
+            paragraph'''
+
+print(paragraph)
+
+str = 'Runoob'
+
+print(str)  # 输出字符串
+print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
+print(str[0])  # 输出字符串第一个字符
+print(str[2:5])  # 输出从第三个开始到第五个的字符
+print(str[2:])  # 输出从第三个开始的后的所有字符
+print(str * 2)  # 输出字符串两次
+print(str + '你好')  # 连接字符串
+
